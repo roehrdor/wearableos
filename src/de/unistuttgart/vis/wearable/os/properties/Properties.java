6f6115698902c8b8cb24956d493eded71e4e56e0
Oliver Röhrdanz <roehrdor@outlook.com>
2015-02-09 23:31:00 +0100

    Added ability to compress files in a given directory and created static class for handling static properties

---
+++ src/de/unistuttgart/vis/wearable/os/properties/Properties.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.vis.wearable.os.properties;
+
+/**
+ * This class is used to store the user properties such as the password for the encryption or the
+ * path of the files.
+ *
+ * @author roehrdor
+ */
+public class Properties {
+    // These are constant right now but could basically be changed dynamically
+    public static java.io.File storageDirectory = new java.io.File("/data/data/de.unistuttgart.vis.wearable.os/files");
+
+}
