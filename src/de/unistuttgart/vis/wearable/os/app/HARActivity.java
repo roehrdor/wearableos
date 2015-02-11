89d6da25adf1801679f70c1886df1c40e9e2cce3
Lucas Röhrle <lucas.roehrle@sgos.de>
2015-02-11 13:15:55 +0100

    The not implemented activities build in that not crash if you click de Button

---
+++ src/de/unistuttgart/vis/wearable/os/app/HARActivity.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.vis.wearable.os.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.unistuttgart.vis.wearable.os.R;
+
+/**
+ * Created by Lucas on 11.02.2015.
+ */
+public class HARActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niy);
+    }
+}
