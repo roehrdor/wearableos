a93ee1f5b092ef06e05988b860bd6b2bde762c36
roehrdor <roehrdor@outlook.com>
2015-02-12 23:45:34 +0100

    Removed unused imports

--- src/de/unistuttgart/vis/wearable/os/internalapi/PSensor.java
+++ src/de/unistuttgart/vis/wearable/os/internalapi/PSensor.java
@@ -10,10 +10,8 @@ package de.unistuttgart.vis.wearable.os.internalapi;
 import de.unistuttgart.vis.wearable.os.graph.GraphType;
 import de.unistuttgart.vis.wearable.os.sensors.MeasurementSystems;
 import de.unistuttgart.vis.wearable.os.sensors.MeasurementUnits;
-import de.unistuttgart.vis.wearable.os.sensors.SensorData;
 import de.unistuttgart.vis.wearable.os.sensors.SensorType;
 import de.unistuttgart.vis.wearable.os.utils.Constants;
-import de.unistuttgart.vis.wearable.os.utils.Utils;
 
 /**
  * Sensor object that can be used in the settings app
