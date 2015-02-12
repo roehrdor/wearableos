3a276df58bcb851171ea8781ff6b080406544835
roehrdor <roehrdor@outlook.com>
2015-02-12 17:22:33 +0100

    Added static fields to have the strings / locations of the service classes for esay creating a service intent to start the service

--- src/de/unistuttgart/vis/wearable/os/properties/Properties.java
+++ src/de/unistuttgart/vis/wearable/os/properties/Properties.java
@@ -1,3 +1,10 @@
+/*
+ * This file is part of the Garment OS Project. For any details concerning use
+ * of this project in source or binary form please refer to the provided license
+ * file.
+ *
+ * (c) 2014-2015 Garment OS
+ */
 package de.unistuttgart.vis.wearable.os.properties;
 
 /**
@@ -9,5 +16,11 @@ package de.unistuttgart.vis.wearable.os.properties;
 public class Properties {
     // These are constant right now but could basically be changed dynamically
     public static java.io.File storageDirectory = new java.io.File("/data/data/de.unistuttgart.vis.wearable.os/files");
+
+    //
+    // These constants are used to start the services
+    //
+    public static final String GARMENT_SERVICE = "de.unistuttgart.vis.wearable.os.service.GarmentOSSerivce";
+    public static final String GARMENT_INTERNAL_SENSOR = "de.unistuttgart.vis.wearable.os.internalservice.GarmentOSServiceInternal";
 
 }
