eddf93234f7985ac1db1335bb96dddf51529b81e
Sophie Ogando <sophie.ogando@gmail.com>
2015-02-17 01:32:28 +0100

    graph in settings app now updates live

--- src/de/unistuttgart/vis/wearable/os/app/GraphActivity.java
+++ src/de/unistuttgart/vis/wearable/os/app/GraphActivity.java
@@ -16,6 +16,7 @@ import de.unistuttgart.vis.wearable.os.api.PSensor;
 public class GraphActivity extends Activity {
 
 	private LinearLayout chart;
+	private Thread chartUpdateThread;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +31,18 @@ public class GraphActivity extends Activity {
 		PSensor sensor = APIFunctions.API_getSensorById(getIntent().getExtras()
 				.getInt("sensorId"));
 
-		chart.addView(GraphRenderer.createGraph(sensor, this));
+		GraphRenderer.ChartThreadTuple tuple = GraphRenderer.createGraph(
+				sensor, this);
+		chart.addView(tuple.getChart());
+		chartUpdateThread = tuple.getThread();
+		chartUpdateThread.start();
+	}
 
+	@Override
+	protected void onDestroy() {
+		if (chartUpdateThread != null)
+			chartUpdateThread.interrupt();
+
+		super.onDestroy();
 	}
 }
--- src/de/unistuttgart/vis/wearable/os/graph/GraphData.java
+++ src/de/unistuttgart/vis/wearable/os/graph/GraphData.java
@@ -3,27 +3,26 @@ package de.unistuttgart.vis.wearable.os.graph;
 import java.util.Date;
 
 public class GraphData {
-    private String title;
-    private Date[] x;
-    private double[] y;    
-    
-    public GraphData(String title, Date[] x, double[] y) {
-	assert(x.length == y.length);
-	this.title = title;
-	this.x = x;
-	this.y = y;
-    }
-    
-    public Date[] getX() {
-	return this.x;
-    }
-    
-    public double[] getY() {
-	return this.y;
-    }
-    
-    public String getTitle() {
-	return this.title;
-    }
-}
+	private String title;
+	private Date[] x;
+	private double[] y;
 
+	public GraphData(String title, Date[] x, double[] y) {
+		assert (x.length == y.length);
+		this.title = title;
+		this.x = x;
+		this.y = y;
+	}
+
+	public Date[] getX() {
+		return this.x;
+	}
+
+	public double[] getY() {
+		return this.y;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+}
