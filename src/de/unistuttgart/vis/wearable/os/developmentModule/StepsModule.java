5fb1de3d586c062a2372a1928abfcbe6e1b3a522
Sophie Ogando <sophie.ogando@gmail.com>
2015-03-03 09:47:18 +0100

    changes in popupmodule and graph module

--- src/de/unistuttgart/vis/wearable/os/developmentModule/StepsModule.java
+++ src/de/unistuttgart/vis/wearable/os/developmentModule/StepsModule.java
@@ -1,6 +1,8 @@
 package de.unistuttgart.vis.wearable.os.developmentModule;
 
 import de.unistuttgart.vis.wearable.os.R;
+import de.unistuttgart.vis.wearable.os.activity.Activity;
+import de.unistuttgart.vis.wearable.os.activityRecognition.ActivityRecognitionModule;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -11,7 +13,8 @@ public class StepsModule extends PopupModule1 {
 		super(context, attrs);
 
 		TextView content = new TextView(context);
-		content.setText("100 Steps today...");
+		
+		
 
 		super.createLayout(context, content, R.drawable.steps, "Steps");
 	}
