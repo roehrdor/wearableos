dca83eb486784dda58dd38fe052855ce5d127f85
Sophie Ogando <sophie.ogando@gmail.com>
2015-03-04 10:29:44 +0100

    changes in temperature and gps module

--- src/de/unistuttgart/vis/wearable/os/developmentModule/GpsModule.java
+++ src/de/unistuttgart/vis/wearable/os/developmentModule/GpsModule.java
@@ -1,40 +1,90 @@
 package de.unistuttgart.vis.wearable.os.developmentModule;
 
+import java.util.Vector;
+
 import de.unistuttgart.vis.wearable.os.R;
+import de.unistuttgart.vis.wearable.os.api.APIFunctions;
+import de.unistuttgart.vis.wearable.os.api.PSensor;
+import de.unistuttgart.vis.wearable.os.graph.GraphData;
+import de.unistuttgart.vis.wearable.os.sensors.Sensor;
+import de.unistuttgart.vis.wearable.os.sensors.SensorData;
+import de.unistuttgart.vis.wearable.os.sensors.SensorType;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class GpsModule extends PopupModule1 {
+	
+	private final int NUMBER_OF_VALUES = 1;
 
 	public GpsModule(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
-		LinearLayout content = new LinearLayout(context);
-		Button button = new Button(context);
+		TextView content = new TextView(context);
+		
+		// Button button = new Button(context);
+		//
+		// button.setText("show in map");
+		// button.setBackgroundColor(Color.parseColor("#606060"));
+		// button.setTextColor(Color.WHITE);
+		//
+		// button.setOnClickListener(new View.OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View view) {
+		//
+		// openMapsActivity();
+		//
+		// }
+		// });
+		//
+		// content.addView(button);
 
-		button.setText("show in map");
-		button.setBackgroundColor(Color.parseColor("#606060"));
-		button.setTextColor(Color.WHITE);
+		PSensor[] gpsSensors = APIFunctions.getAllSensors(SensorType.GPS_SENSOR);
+		
+		
+		
+		
+		PSensor gpsSensor = gpsSensors[0];		
 
-		button.setOnClickListener(new View.OnClickListener() {
 
-			@Override
-			public void onClick(View view) {
+		Vector<SensorData> gpsData = gpsSensor.getRawData(NUMBER_OF_VALUES);
+		
 
-				openMapsActivity();
+		if(gpsData.isEmpty()) {
+			content.setText("no Data");
+		} else {
+			content.setText(gpsData.get(0).getData()[0] + "");
+		}
+			
+		
+			
+		
+			
+		
 
-			}
-		});
-
-		content.addView(button);
-
-		super.createLayout(context, content, R.drawable.gps, "GPS");
-	}
-
+//		for (PSensor sensor : APIFunctions.getAllSensors()) {
+//			if (sensor != null) {
+//				if (sensor.getSensorType() == SensorType.GPS_SENSOR) {
+//					gpsSensor = sensor;
+//				
+//                 Vector<SensorData> data = gpsSensor.getLastRawData();
+//                 
+//                 content.setText((CharSequence) data);
+//					
+//				}
+//			}
+//			
+			
+			
+			
+			super.createLayout(context, content, R.drawable.gps, "GPS");
+		}
+	
 	public void openMapsActivity() {
 
 		// Context context = getContext();
--- src/de/unistuttgart/vis/wearable/os/developmentModule/TemperatureModule.java
+++ src/de/unistuttgart/vis/wearable/os/developmentModule/TemperatureModule.java
@@ -1,26 +1,39 @@
 package de.unistuttgart.vis.wearable.os.developmentModule;
 
-import de.unistuttgart.vis.wearable.os.R;
+import java.util.Vector;
 
+import de.unistuttgart.vis.wearable.os.R;
+import de.unistuttgart.vis.wearable.os.api.APIFunctions;
+import de.unistuttgart.vis.wearable.os.api.PSensor;
+import de.unistuttgart.vis.wearable.os.sensors.SensorData;
+import de.unistuttgart.vis.wearable.os.sensors.SensorType;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
 public class TemperatureModule extends PopupModule1 {
 
+	private final int NUMBER_OF_VALUES = 1;
+
 	public TemperatureModule(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 		TextView content = new TextView(context);
-		content.setText("20 degree right now...");
 
-		// try {
-		// SettingsModule.getInstance().getSensorData(sensorName, raw);
-		// } catch (MissingSensorPropertyException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		//
+		PSensor[] temperatureSensors = APIFunctions
+				.getAllSensors(SensorType.TEMPERATURE);
+
+		PSensor temperatureSensor = temperatureSensors[0];
+
+		Vector<SensorData> temperatureData = temperatureSensor
+				.getRawData(NUMBER_OF_VALUES);
+
+		if (temperatureData.isEmpty()) {
+			content.setText("no Data");
+		} else {
+			content.setText(temperatureData.get(0).getData()[0] + "");
+		}
+
 		super.createLayout(context, content, R.drawable.temperature,
 				"Temperature");
 	}
