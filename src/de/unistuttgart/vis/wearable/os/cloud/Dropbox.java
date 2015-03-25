f7fcc280dbca38a5b33345896ba6ed65ccd1653d
Martin Root <martin.root@ymail.com>
2015-03-25 11:55:39 +0100

    fixed error with import

--- src/de/unistuttgart/vis/wearable/os/cloud/Dropbox.java
+++ src/de/unistuttgart/vis/wearable/os/cloud/Dropbox.java
@@ -267,92 +267,99 @@ public class Dropbox extends Activity {
                     if (outputStream != null) {
 
 
-                            if (!Archiver.notEncryptedGOSFile(file) && !abort) {
+                        if (!Archiver.notEncryptedGOSFile(file) && !abort) {
 
-                                AlertDialog.Builder alert = new AlertDialog.Builder(Dropbox.this);
+                            AlertDialog.Builder alert = new AlertDialog.Builder(Dropbox.this);
 
-                                alert.setTitle("Please enter password:");
-                                final EditText input = new EditText(Dropbox.this);
-                                alert.setView(input);
+                            alert.setTitle("Please enter password:");
+                            final EditText input = new EditText(Dropbox.this);
+                            alert.setView(input);
 
-                                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        key = input.getText().toString();
-                                        int value =Archiver.unpackEncryptedFile(key, file);
-                                        switch (value) {
-                                            case Constants.UNPACK_NO_ERROR:
-                                                Toast.makeText(context,
-                                                        "File import finished",
-                                                        Toast.LENGTH_SHORT).show();
-                                                finish();
-                                                break;
-                                            case Constants.UNPACK_INVALID_FILE:
-                                                Toast.makeText(context,
-                                                        "Invalid File",
-                                                        Toast.LENGTH_SHORT).show();
-                                                finish();
-                                                break;
-                                            case Constants.UNPACK_EXTRACTING_FAILED:
-                                                Toast.makeText(context,
-                                                        "Extracting failed",
-                                                        Toast.LENGTH_SHORT).show();
-                                                finish();
-                                                break;
-                                            case Constants.UNPACK_WRONG_KEY:
-                                                Toast.makeText(context,
-                                                        "Wrong Password",
-                                                        Toast.LENGTH_SHORT).show();
-                                                finish();
-                                                break;
+                            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    key = input.getText().toString();
+                                    int value = Archiver.unpackEncryptedFile(key, file);
+                                    switch (value) {
+                                        case Constants.UNPACK_NO_ERROR:
+                                            Toast.makeText(context,
+                                                    "File import finished",
+                                                    Toast.LENGTH_SHORT).show();
+                                            finish();
+                                            file.delete();
+                                            break;
+                                        case Constants.UNPACK_INVALID_FILE:
+                                            Toast.makeText(context,
+                                                    "Invalid File",
+                                                    Toast.LENGTH_SHORT).show();
+                                            file.delete();
+                                            finish();
+                                            break;
+                                        case Constants.UNPACK_EXTRACTING_FAILED:
+                                            Toast.makeText(context,
+                                                    "Extracting failed",
+                                                    Toast.LENGTH_SHORT).show();
+                                            file.delete();
+                                            finish();
+                                            break;
+                                        case Constants.UNPACK_WRONG_KEY:
+                                            Toast.makeText(context,
+                                                    "Wrong Password",
+                                                    Toast.LENGTH_SHORT).show();
+                                            file.delete();
+                                            finish();
+                                            break;
 
-                                        }
                                     }
-                                });
-
-                                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        finished = false;
-                                        file.delete();
-                                        finish();
-                                    }
-                                });
-                                alert.setCancelable(false);
-                                alert.show();
-                            } else if (!abort) {
-                                int value = APIFunctions.unpackArchiveFile(file);
-
-                                switch (value) {
-                                    case Constants.UNPACK_NO_ERROR:
-                                        Toast.makeText(context,
-                                                "File import finished",
-                                                Toast.LENGTH_SHORT).show();
-                                        finish();
-                                        break;
-                                    case Constants.UNPACK_INVALID_FILE:
-                                        Toast.makeText(context,
-                                                "Invalid File",
-                                                Toast.LENGTH_SHORT).show();
-                                        finish();
-                                        break;
-                                    case Constants.UNPACK_EXTRACTING_FAILED:
-                                        Toast.makeText(context,
-                                                "Extracting failed",
-                                                Toast.LENGTH_SHORT).show();
-                                        finish();
-                                        break;
-                                    case Constants.UNPACK_WRONG_KEY:
-                                        Toast.makeText(context,
-                                                "Wrong Password",
-                                                Toast.LENGTH_SHORT).show();
-                                        finish();
-                                        break;
-
                                 }
+                            });
+
+                            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    finished = false;
+                                    file.delete();
+                                    finish();
+                                }
+                            });
+                            alert.setCancelable(false);
+                            alert.show();
+                        } else if (!abort) {
+                            int value = APIFunctions.unpackArchiveFile(file);
+
+                            switch (value) {
+                                case Constants.UNPACK_NO_ERROR:
+                                    Toast.makeText(context,
+                                            "File import finished",
+                                            Toast.LENGTH_SHORT).show();
+                                    file.delete();
+                                    finish();
+                                    break;
+                                case Constants.UNPACK_INVALID_FILE:
+                                    Toast.makeText(context,
+                                            "Invalid File",
+                                            Toast.LENGTH_SHORT).show();
+                                    file.delete();
+                                    finish();
+                                    break;
+                                case Constants.UNPACK_EXTRACTING_FAILED:
+                                    Toast.makeText(context,
+                                            "Extracting failed",
+                                            Toast.LENGTH_SHORT).show();
+                                    file.delete();
+                                    finish();
+                                    break;
+                                case Constants.UNPACK_WRONG_KEY:
+                                    Toast.makeText(context,
+                                            "Wrong Password",
+                                            Toast.LENGTH_SHORT).show();
+                                    file.delete();
+                                    finish();
+                                    break;
+
                             }
                         }
                     }
+                }
             });
-            file.delete();
             return;
         }
 
@@ -510,6 +517,10 @@ public class Dropbox extends Activity {
                                 @Override
                                 public void onItemClick(AdapterView<?> parent, View view,
                                                         int position, long id) {
+                                    if (!internetAvailable()){
+                                        Toast.makeText(getBaseContext(),"Please activate wifi or mobile Data",Toast.LENGTH_SHORT).show();
+                                        finish();
+                                    }
                                     if (list.getItemAtPosition(position).toString().endsWith(".zip")) {
                                         finished = false;
                                         abort = false;
@@ -568,6 +579,10 @@ public class Dropbox extends Activity {
 
     @Override
     public void onBackPressed() {
+        if (!internetAvailable()){
+            Toast.makeText(getBaseContext(),"Please activate wifi or mobile Data",Toast.LENGTH_SHORT).show();
+            finish();
+        }
         if (currentEntry== null){
             super.onBackPressed();
         }
@@ -577,6 +592,7 @@ public class Dropbox extends Activity {
         } else {
             super.onBackPressed();
         }
+
     }
 
 
@@ -586,6 +602,10 @@ public class Dropbox extends Activity {
      * @param view
      */
     public void upload(View view) {
+        if (!internetAvailable()){
+            Toast.makeText(getBaseContext(),"Please activate wifi or mobile Data",Toast.LENGTH_SHORT).show();
+            finish();
+        }
             finished = false;
             abort = true;
             uploadTask = new Upload();
